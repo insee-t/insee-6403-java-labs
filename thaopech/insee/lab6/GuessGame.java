3d4c90a3f350f60cafdb34c8a6b53a5472a040d7
insee-t <insee.t@kkumail.com>
2024-12-27 16:59:18 +0700

    Lab 6 initial commit

---
+++ thaopech/insee/lab6/GuessGame.java
@@ -0,0 +1,120 @@
+package thaopech.insee.lab6;
+
+import java.util.*;
+
+public class GuessGame {
+
+    public static Scanner input = new Scanner(System.in);
+
+    private int min;
+    private int max;
+    private int maxTries;
+    private int answer;
+    private int attempts;
+
+    // Constructors
+    public GuessGame() {
+        min = 1;
+        max = 100;
+        maxTries = 10;
+        answer  = 0;
+        attempts = 0;
+    }
+
+    public GuessGame(int min, int max, int maxTries) {
+        this.min = min;
+        this.max = max;
+        this.maxTries = maxTries;
+
+        answer  = 0;
+        attempts = 0;
+    }
+
+    public int getMin() {
+        return this.min;
+    }
+
+    public void setMin(int min) {
+        this.min = min;
+    }
+
+    public int getMax() {
+        return this.max;
+
+    }
+
+    public void setMax(int max) {
+        this.max = max;
+    }
+
+    public int getMaxTries() {
+        return this.maxTries;
+    }
+
+    public void setMaxTries(int maxTries) {
+        this.maxTries = maxTries;
+    }
+    // public void configureGame(int min, int max, int maxTries) {
+
+    //     System.out.print("Enter the min value:");
+    //     min = scanner.nextInt();
+
+    //     max = getMax();
+
+    //     numTries = getNumTries();
+    // }
+    //
+    public int getAttempts() {
+        return this.attempts;
+    }
+    public int getAnswer() {
+        return this.answer;
+    }
+    public void generateAnswer() {
+        int min = getMin();
+        int max = getMax();
+        this.answer = min + (int) (Math.random() * ((max - min) + 1));
+    }
+
+    public int getUserGuess() {
+        System.out.print(String.format("Enter an integer between %d and %d: ", min, max));
+        return input.nextInt();
+    }
+
+
+    public int numberGuessingResponse(int userGuess, int index) {
+
+        int answer = getAnswer();
+        if (userGuess == answer) {
+            System.out.println("Congratulations! You've guessed  the number in " + index + (index == 1 ? " attempts" : " attempt"));
+            return 0;
+        }
+        if (userGuess > answer) {
+            System.out.println("Try a lower number!");
+            return 1;
+        }
+        System.out.println("Try a higher number!");
+        return 1;
+    }
+
+    public boolean playSingleGame() {
+
+        System.out.println("Welcome to a number guessing game!");
+        generateAnswer();
+
+        for (int guessIndex = 1; guessIndex <= maxTries; guessIndex++) {
+            int userGuess = getUserGuess();
+            while (userGuess < min || userGuess > max) {
+                System.err.println(String.format("The number must be between %d and %d", min, max));
+                userGuess = getUserGuess();
+            }
+
+            if (numberGuessingResponse(userGuess, guessIndex) == 0) {
+                return true;
+            }
+        }
+
+        System.out.println("Sorry, you've used all your attempts. The correct answer was: " + this.answer);
+        return false;
+    }
+}
---
+++ thaopech/insee/lab6/NumberGuessingOOPGame.java
@@ -0,0 +1,44 @@
+        // fix the format
+        // ? Make sure they're instance though.
+package thaopech.insee.lab6;
+
+import java.util.*;
+
+public class NumberGuessingOOPGame {
+
+    private GuessGame game;
+    public static Scanner input = new Scanner(System.in);
+
+    // Figure 2
+    public void configure() {
+        System.out.print("Enter the min value: ");
+        int min = input.nextInt();
+        System.out.print("Enter the max value: ");
+        int max = input.nextInt();
+        System.out.print("Enter the maximum number of tries: ");
+        int maxTries = input.nextInt();
+
+        this.game = new GuessGame(min, max, maxTries);
+    }
+
+    // Figure 3
+    public void playGames() {
+        boolean playAgain;
+        do {
+            boolean result = this.game.playSingleGame();
+            System.out.println(result ? "You win!" : "Better luck next time.");
+            System.out.print("Do you want to play again? (y\\n): ");
+            playAgain = input. next().equalsIgnoreCase("y");
+            if (playAgain) {
+                configure();
+            }
+        } while (playAgain);
+
+        System.out.println("Thank you for playing the Number Guessing Game!");
+    }
+    public static void main(String[] args) {
+        NumberGuessingOOPGame program = new NumberGuessingOOPGame();
+        program.configure();
+        program.playGames();
+    }
+}
